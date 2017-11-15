83908a30b416b461873a1b25cab67ad5f68dcb56
jaceksen <jaceksen@gmail.com>
2017-11-15 22:43:18 +0100

    REST CRUD

--- src/main/java/pl/jaceksen/messenger/resources/MessageResource.java
+++ src/main/java/pl/jaceksen/messenger/resources/MessageResource.java
@@ -2,7 +2,11 @@ package pl.jaceksen.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,10 +35,33 @@ public class MessageResource {
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Message getSingleMessage(@PathParam("messageId") long messageId){
 		return messageService.getMessage(messageId);
-	
 	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message  message){
+		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id, Message message){
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId") long id){
+		messageService.removeMessage(id);
+	}
+	
 
 }
